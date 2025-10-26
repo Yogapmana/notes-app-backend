f2e0b42165424b88036e4de6f50e6d8cc5e62dca
Yogapmana <yogapermana402@gmail.com>
2025-10-26 22:05:49 +0700

    menambah fitur export notes

--- src/server.js
+++ src/server.js
@@ -1,115 +1,125 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Hapi = require('@hapi/hapi');
-const Jwt = require('@hapi/jwt');
+const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 
-const notes = require('./api/notes');
-const NotesService = require('./services/postgres/NotesService');
-const NotesValidator = require('./validator/notes');
+const notes = require("./api/notes");
+const NotesService = require("./services/postgres/NotesService");
+const NotesValidator = require("./validator/notes");
 
-const users = require('./api/users');
-const UsersService = require('./services/postgres/UsersService');
-const UsersValidator = require('./validator/users');
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 
-const authentications = require('./api/authentications');
-const AuthenticationsService = require('./services/postgres/AuthenticationsService');
-const TokenManager = require('./tokenize/TokenManager');
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
 
-const collaborations = require('./api/collaborations');
-const CollaborationsService = require('./services/postgres/CollaborationsService');
-const CollaborationsValidator = require('./validator/collaborations');
+const collaborations = require("./api/collaborations");
+const CollaborationsService = require("./services/postgres/CollaborationsService");
+const CollaborationsValidator = require("./validator/collaborations");
 
-const AuthenticationsValidator = require('./validator/authentications');
-const ClientError = require('./exceptions/ClientError');
+const _exports = require("./api/exports");
+const ProducerService = require("./services/rabbitmq/ProducerService");
+const ExportValidator = require("./validator/exports");
 
+const AuthenticationsValidator = require("./validator/authentications");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
-    const collaborationsService = new CollaborationsService();
-    const notesService = new NotesService(collaborationsService);
-    const usersService = new UsersService();
-    const authenticationsService = new AuthenticationsService();
+  const collaborationsService = new CollaborationsService();
+  const notesService = new NotesService(collaborationsService);
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
-    const server = Hapi.server({
-        port: process.env.PORT,
-        host: process.env.HOST,
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
 
-    await server.register([
-        { 
-            plugin: Jwt, 
-        },
-    ]);
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
 
-    server.auth.strategy('notesapp_jwt', 'jwt', {
-        keys: process.env.ACCESS_TOKEN_KEY,
-        verify: {
-            aud: false,
-            iss: false,
-            sub: false,
-            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
-        },
-        validate: (artifacts) => ({ 
-            isValid: true,
-            credentials: {
-                id: artifacts.decoded.payload.id,
-            },
-        }),
-    });
+  server.auth.strategy("notesapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
 
-    await server.register([
-        {   
-            plugin: notes,
-            options: {
-                service: notesService,
-                validator: NotesValidator,
-            },
-        },
-        {
-            plugin: users,
-            options: {
-                service: usersService,
-                validator: UsersValidator,
-            },
-        },
-        {
-            plugin: authentications,
-            options: {
-                authenticationsService,
-                usersService,
-                tokenManager: TokenManager,
-                validator: AuthenticationsValidator,
-            },
-        },
-        {
-            plugin: collaborations,
-            options: {
-                collaborationsService,
-                notesService,
-                validator: CollaborationsValidator,
-            },
-        }
-    ]);
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
+    {
+      plugin: collaborations,
+      options: {
+        collaborationsService,
+        notesService,
+        validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportValidator,
+      },
+    },
+  ]);
 
-    server.ext('onPreResponse', (request, h) => {
-        const { response } = request;
-        if (response instanceof ClientError) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: response.message,
-            });
-            newResponse.code(response.statusCode);
-            return newResponse;
-        }
-        return h.continue;
-    });
+  server.ext("onPreResponse", (request, h) => {
+    const { response } = request;
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    return h.continue;
+  });
 
-    await server.start();
-        console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
